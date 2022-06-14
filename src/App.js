93d8aeca0b6af1d87cd595d002bc624821bdf7e7
jackson-ribeiro <jacksonquest1@gmail.com>
2022-06-14 10:31:50 -0300

    Estruturação de Arquivos e Pastas

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { Footer, Blog, Possibility, Features, HighfiveEducation, Header } from './containers';
+import { CTA, Brand, Navbar, Feature } from './components';
+import './App.css';
+
+
+const App = () => {
+    return (
+        <div className="App">
+            <div className="gradient__bg">
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <HighfiveEducation />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <Footer />
+        </div>
+    )
+}
+
+export default App
---
+++ src/containers/highfiveEducation/HighfiveEducation.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './highfiveEducation.css';
+
+const HighfiveEducation = () => {
+  return (
+    <div>
+        HighFive Education
+    </div>
+  )
+}
+
+export default HighfiveEducation
