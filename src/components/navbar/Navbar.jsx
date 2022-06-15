e6eb7ab82673c1228b706d39aa7ba95a999da685
jackson-ribeiro <jacksonquest1@gmail.com>
2022-06-15 06:04:38 -0300

    Navbar Finalizada

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
-import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/rabot.svg';
 import './navbar.css';
 
+const Menu = () => (
+  <>
+  <p><a href="#inicio">Início</a></p>
+  <p><a href="#whfe">HighFive Education</a></p>
+  <p><a href="#possibility">Inteligência Artificial</a></p>
+  <p><a href="#features">Estudo de Casos</a></p>
+  <p><a href="#blog">Biblioteca</a></p>
+  </>
+)
+
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
   return (
     <div className="highfive__navbar">
       <div className="highfive__navbar-links">
@@ -11,16 +22,29 @@ const Navbar = () => {
           <img src= {logo} alt="logo" />
         </div>
         <div className="highfive__navbar-links_container">
-          <p><a href="#inicio">Início</a></p>
-          <p><a href="#whfe">HighFive Education</a></p>
-          <p><a href="#possibility">Inteligência Artificial</a></p>
-          <p><a href="#features">Estudo de Casos</a></p>
-          <p><a href="#blog">Biblioteca</a></p>
+          <Menu />
         </div>
       </div>
       <div className="highfive__navbar-sign">
         <p>Entrar</p>
-        <button type="button"></button>
+        <button type="button">Cadastre-se</button>
+      </div>
+      <div className="highfive__navbar-menu">
+        {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)}/>
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)}/>
+        }
+        {toggleMenu && (
+          <div className="highfive__navbar-menu_container scale-up-center">
+            <div className="highfive__navbar-menu_container-links">
+              <Menu />
+              <div className="highfive__navbar-menu_container-links-sign">
+                <p>Entrar</p>
+                <button type="button">Cadastre-se</button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
